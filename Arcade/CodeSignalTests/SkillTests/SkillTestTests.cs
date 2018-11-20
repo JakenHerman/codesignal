039972dde98254129fbce31a3a42efc8d362ddde
= <jakenherman7@gmail.com>
2018-11-20 12:26:20 -0600

    Started new filepath SkillTests to house skilltest challenges

---
+++ Arcade/CodeSignalTests/SkillTests/SkillTestTests.cs
@@ -0,0 +1,24 @@
+﻿using SkillTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeSignalTests
+{
+    [TestClass()]
+    public class SkillTestTests
+    {
+        [TestMethod]
+        public void TestIsLucky()
+        {
+            Assert.IsTrue(STMain.IsLucky(1230));
+            Assert.IsFalse(STMain.IsLucky(239017));
+            Assert.IsTrue(STMain.IsLucky(134008));
+            Assert.IsFalse(STMain.IsLucky(10));
+            Assert.IsTrue(STMain.IsLucky(11));
+            Assert.IsTrue(STMain.IsLucky(1010));
+            Assert.IsFalse(STMain.IsLucky(261534));
+            Assert.IsFalse(STMain.IsLucky(100000));
+            Assert.IsTrue(STMain.IsLucky(999999));
+            Assert.IsTrue(STMain.IsLucky(123321));
+        }
+    }
+}
---
+++ Arcade/SkillTests/STMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SkillTests
+{
+    public class STMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsLucky(1230)); // should return true
+        }
+
+        /* Ticket numbers usually consist of an even number of digits.
+         * A ticket number is considered lucky if the sum of the first 
+         * half of the digits is equal to the sum of the second half.
+         * 
+         * Given a ticket number n, determine if it's lucky or not.
+         */
+        public static bool IsLucky(int n)
+        {
+            List<int> listOfInts = new List<int>();
+            while (n > 0)
+            {
+                listOfInts.Add(n % 10);
+                n = n / 10;
+            }
+            listOfInts.Reverse();
+
+            var first = 0;
+            for (int i = 0; i < listOfInts.Count / 2; i++)
+            {
+                first += listOfInts.ElementAt(i);
+            }
+
+            var second = 0;
+            for (int i = listOfInts.Count / 2; i < listOfInts.Count; i++)
+            {
+                second += listOfInts.ElementAt(i);
+            }
+
+            return first == second;
+        }
+    }
+}
